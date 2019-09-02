55d1a54f16d54f863773b387de4f1a0206e2ab4d
Eugene Lykhovyd <lykhovyd@gmail.com>
2019-09-02 22:26:51 +0000

    lwt support

--- ocamlprob.ml
+++ ocamlprob.ml
@@ -8,4 +8,16 @@ let () =
   with exn -> printfn "Error: %s" (Exn.str exn) end;
   match !configfile with 
   | None -> printfn "Please specify config file. Use -help for help."
-  | Some file -> (printfn "Got file %s" file; Daemon.manage())
+  | Some _file ->
+    Daemon.manage();
+    (* parse config *)
+    (* run check according to config *)
+    let config_check () =
+       Lwt.return_unit
+    in
+    let main f =
+      try%lwt
+        Lwt_util.timely_loop ~immediate:false 1. f
+      with Daemon.ShouldExit -> Lwt.return_unit
+    in
+    Lwt_main.run @@ main config_check
